6aceee819cb793ada370d1d9a8818d5450a5a2a2
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-20 18:35:30 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -25,13 +25,12 @@ class InteractiveRecord
       self.send("#{property}=", value)
     end
   end
-  
+
   def self.create_table
   sql =  <<-SQL
-    CREATE TABLE IF NOT EXISTS dogs (
+    CREATE TABLE IF NOT EXISTS songs (
       id INTEGER PRIMARY KEY,
-      name TEXT,
-      breed TEXT
+      name TEXT
       )
   SQL
   DB[:conn].execute(sql)
